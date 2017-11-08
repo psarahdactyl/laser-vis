0cbd5827c479ae0c750fae5cda07fc41df79b673
sarahanne <sak@dgp.toronto.edu>
2017-11-08 00:00:01 -0500

    almost doneeeeee the image analogies

--- analogies.py
+++ analogies.py
@@ -44,11 +44,11 @@ def compute_features(image):
     if h-2 > 0 and w-2 > 0:
         f_3x3 = np.ones((h-2,w-2,rgby.shape[2]*9))
     else:
-        f_3x3 = np.ones((2,2,rgby.shape[2]*9))
+        f_3x3 = np.ones((3,3,rgby.shape[2]*9))
     if h-4 > 0 and w-4 > 0:
         f_5x5 = np.ones((h-4,w-4,rgby.shape[2]*25))
     else:
-        f_5x5 = np.ones((4,4,rgby.shape[2]*25))
+        f_5x5 = np.ones((5,5,rgby.shape[2]*25))
 
     # go through image and get features by neighboorhoods
     h,w,c = rgby.shape
@@ -63,46 +63,68 @@ def compute_features(image):
 
     return (f_3x3, f_5x5)
 
+def reshape(matrix):
+    f = matrix
+    h,w,c = f.shape
+    f = f.reshape(h*w,c)
+    return f
+
 def combine_features(list_of_features):
-    combined = np.ones((list_of_features[0]).shape) # all feature matrices should be the same shape
-    for m in list_of_features:
-        combined = np.concatenate((combined,m), axis=2)
+    if len(list_of_features) == 1:
+        current_layer = list_of_features[0][1]
+        reshaped_current_layer = reshape(current_layer)
+        combined = reshaped_current_layer
+
+    else:
+        current_layer = list_of_features[0][1] # 5x5
+        c_c = current_layer.shape[2]
+        previous_layer = list_of_features[1][0] # 3x3
+        p_c = previous_layer.shape[2]
+        g = (c_c - p_c) / 2
+
+        previous_layer = np.lib.pad(previous_layer, ((1,1),(1,1),(g,g)), 'constant', constant_values=0)
+        reshaped_current_layer = reshape(current_layer)
+        reshaped_previous_layer = reshape(previous_layer)
+
+        #print(reshaped_current_layer.shape, reshaped_previous_layer.shape)
+
+        combined = np.concatenate((reshaped_current_layer, reshaped_previous_layer), axis=1)
+        print(combined)
 
     print("combined")
     print(type(combined))
+    print(combined.shape)
     return combined
 
 def make_search_structure(features, level):
     #lshf = LSHForest(n_estimators=20, n_candidates=200, n_neighbors=1).fit(f)
     #h,w,c = features.shape
     f = features
-    h,w,c = f.shape
-    f = f.reshape(h*w,c)
     nbrs = NearestNeighbors(n_neighbors=1, algorithm='brute').fit(f)
-    return nbrs, f
+    return nbrs
 
 def find_3x3_N(matrix, point):
     i,j = point
     if i-3 < 0 or i+3 > matrix.shape[0]:
-        return None
+        return 0
     elif j-3 < 0 or j+3 > matrix.shape[1]:
-        return None
+        return 0
     return matrix[np.ix_([i-1,i,i+1],[j-1,j,j+1])]
 
 def find_5x5_N(matrix, point):
     i,j = point
     if i-5 < 0 or i+5 > matrix.shape[0]:
-        return None
+        return 0
     elif j-5 < 0 or j+5 > matrix.shape[1]:
-        return None
+        return 0
     return matrix[np.ix_([i-2,i-1,i,i+1,i+2],[j-2,j-1,j,j+1,j+2])]
 
 def find_half_5x5_N(matrix, point):
     i,j = point
     if i-5 < 0 or i+5 > matrix.shape[0]:
-        return None
+        return 0
     elif j-5 < 0 or j+5 > matrix.shape[1]:
-        return None
+        return 0
     N = matrix[np.ix_([i-2,i-1,i,i+1,i+2],[j-2,j-1,j,j+1,j+2])]
     N = N.flatten()
     return N[:12]
@@ -110,27 +132,38 @@ def find_half_5x5_N(matrix, point):
 
 # concatenates a 3x3 neighborhood and a 5x5 neighborhood
 def F_l(point, layer, image_pyramid):
+    l = layer
     if l is not 0:
         small = find_3x3_N(image_pyramid[l], point) + find_3x3_N(image_pyramid[l-1], point)
         large = find_5x5_N(image_pyramid[l], point) + find_5x5_N(image_pyramid[l-1], point)
     else:
         small = find_3x3_N(image_pyramid[l], point)
         large = find_5x5_N(image_pyramid[l], point)
-    return small.flatten() + large.flatten()
+    if small is not 0 and large is not 0:
+        return small.flatten() + large.flatten()
+    else:
+        return None
 
-def find_best_match(s, l, q, point, image_pyramids, search_structure):
+def find_best_match(s, l, point, image_pyramids, search_structure):
     # find matches
-    p_app = best_approx_match(l, q, search_structure, image_pyramids)
-    p_coh = best_coherence_match(s, l, q, point, features, image_pyramids)
+    q = F_l(point, l, image_pyramids[2])
+    if q is None:
+        return None, None
+    print(q)
+    print(q.shape)
+
+    p_app, point_index = best_approx_match(l, q, search_structure, image_pyramids)
+    p_coh, point_index = best_coherence_match(s, l, q, point, features, image_pyramids)
 
     # compute the weighted distance of neighboorhood features with a gaussian kernel
-    d_app = np.norm(F_l(p_app, l, image_pyramid[3]) - F_l(q, l, image_pyramid[3]))
-    d_coh = np.norm(F_l(p_coh, l, image_pyramid[3]) - F_l(q, l, image_pyramid[3]))
+    d_app = np.norm(F_l(p_app, l, image_pyramids[3]) - F_l(q, l, image_pyramids[3]))
+    d_coh = np.norm(F_l(p_coh, l, image_pyramids[3]) - F_l(q, l, image_pyramids[3]))
+
     kappa = 10
     if d_coh < d_app * (1 + kappa * pow(2, (l))):
-        return p_coh
+        return p_coh, point_index
     else:
-        return p_app
+        return p_app, point_index
 
 def best_approx_match(l, q, search_structure, image_pyramids):
     p_i = search_structure.kneighbors([q], 1, return_distance=False)
@@ -140,7 +173,7 @@ def best_approx_match(l, q, search_structure, image_pyramids):
     point_index = (int(p_i/w), p_i%w)
     p_app = a_prime[point_index[0]][point_index[1]]
     if p == p_app:
-        return p_app
+        return p_app, point_index
     else:
         print('help')
 
@@ -152,7 +185,7 @@ def best_coherence_match(s, l, q, point, features, image_pyramids):
             F_l((s[point[0][point[1]]] + (q-r)), l, image_pyramid[3]) - 
             F_l(q, l, image_pyramid[3])) for r in N])
     p_coh = s[r_star] + (q - r_star)
-    return p_coh
+    return p_coh, r_star
 
 # create image analogy
 def get_b_prime(a, a_prime, b):
@@ -200,6 +233,7 @@ def get_b_prime(a, a_prime, b):
 
     b_prime = np.ones((b_features[num_levels][1]).shape)
     b_prime_img_pyr = list()
+    #search_structures = list()
 
     print("Creating B'...")
     # find best match at every level
@@ -215,23 +249,28 @@ def get_b_prime(a, a_prime, b):
         # initialize search structures for approximate nearest neighbor
         print("... initializing search structure (NearestNeighbors)")
         if l == 0:
-            f = a_features[l][1] # 5x5
-            search_structure, reshaped_features = make_search_structure(f, l)
+            f = combine_features([a_features[l]]) # 5x5
+            search_structure = make_search_structure(f, l)
         else:
             f = combine_features([a_features[l], a_features[l-1]])
-            search_structure, reshaped_features = make_search_structure(f, l)
+            search_structure = make_search_structure(f, l)
 
+        #search_structures.append(search_structure)
         image_pyramids.append(b_prime_img_pyr)
 
         print("... finding best pixel match")
         for i,row in enumerate(b_prime_l):
             for j,q in enumerate(row):
-                #s, l, q, point, image_pyramids, search_structure)
+                #(s, l, point, image_pyramids, search_structure)
                 #TODO: TODO: TODO: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! combine features for query point from B
-                point, point_index = find_best_match(source_l, l, b[i][j], (i,j), 
-                    image_pyramids, search_structure)
+                if search_structure is None or image_pyramids is None or source_l is None:
+                    print('what')
+                point, point_index = find_best_match(source_l, l, (i,j), image_pyramids, search_structure)
+                if point is None:
+                    b_prime_l[i][j] = np.array([255,255,255])
                 b_prime_l[i][j] = point
                 source_l[i][j] = point_index
+
         b_prime_img_pyr.append(b_prime_l)
 
         print("----------------------------")
