e2bf795820c475d2ec23edd09015235eb76539b7
sarahanne <sak@dgp.toronto.edu>
2018-03-26 19:00:32 -0400

    explode view by continous connected components

--- explode-view/explode.py
+++ explode-view/explode.py
@@ -2,70 +2,78 @@ import cv2
 import numpy as np
 
 def get_components(src):
-	src_bw = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    src_bw = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
-	connectivity = 8
+    connectivity = 8
 
-	ret, thresh = cv2.threshold(src_bw,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-	output = cv2.connectedComponentsWithStats(thresh, connectivity, cv2.CV_32S)
+    ret, thresh = cv2.threshold(src_bw,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    output = cv2.connectedComponentsWithStats(thresh, connectivity, cv2.CV_32S)
 
-	return output
+    return output
 
 
 def get_labeled_img(connected_comps):
-	output = connected_comps
+    output = connected_comps
 
-	# The first cell is the number of labels
-	num_labels = output[0]
+    # The first cell is the number of labels
+    num_labels = output[0]
 
-	# The second cell is the label matrix
-	labels = output[1]
+    # The second cell is the label matrix
+    labels = output[1]
 
-	# The third cell is the stat matrix
-	stats = output[2]
+    # The third cell is the stat matrix
+    stats = output[2]
 
-	# The fourth cell is the centroid matrix
-	centroids = output[3]
+    # The fourth cell is the centroid matrix
+    centroids = output[3]
 
-	# Map component labels to hue val
-	label_hue = np.uint8(179*labels/np.max(labels))
-	blank_ch = 255*np.ones_like(label_hue)
-	labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+    # Map component labels to hue val
+    label_hue = np.uint8(179*labels/np.max(labels))
+    blank_ch = 255*np.ones_like(label_hue)
+    labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
 
-	# cvt to BGR for display
-	labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+    # cvt to BGR for display
+    labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
 
-	# set bg label to black
-	labeled_img[label_hue==0] = 255
+    # set bg label to black
+    labeled_img[label_hue==0] = 255
 
-	cv2.imwrite('components.png', labeled_img)
+    cv2.imwrite('components.png', labeled_img)
 
-	nodes = dict()
-	for i in range(num_labels):
-		nodes[i] = labeled_img[label_hue==i]
+    nodes = dict()
+    for i in range(num_labels):
+        nodes[i] = labeled_img[label_hue==i]
 
-	output = (nodes,labels,labeled_img)
+    output = (nodes,labels,labeled_img)
 
-	return output
+    return output
 
 
 def organize_components(nodes, labels, labeled_img):
-	graph = dict()
-	label_nums = list(nodes.keys())
-	graph[0] = 0
-	for i in label_nums:
-		print(i)
-		background_mask = np.uint8(labels==0)
-		mask_i = np.uint8(labels==label_nums[i])
-		#mask_iplus1 = np.uint8(labels==label_nums[i+1])
-		new_mask = background_mask+mask_i
-		output = cv2.connectedComponentsWithStats(new_mask, 8, cv2.CV_32S)
-		print(output[0])
-		if output[0] < 3:
-			graph[i] = 2
+    graph = dict()
+    graph[0] = 0
+    # graph will have form
+    # graph[level number] : {comp0, comp1, comp2, ...}
 
-#if j in nodes.keys():
+    label_nums = list(nodes.keys())
+    
+    background_mask = np.uint8(labels==0)
+    for i in label_nums:
+        print(i)
 
+        mask_i = np.uint8(labels==label_nums[i])
+
+        #mask_iplus1 = np.uint8(labels==label_nums[i+1])
+        new_mask = background_mask+mask_i
+        output = cv2.connectedComponentsWithStats(new_mask, 8, cv2.CV_32S)
+
+        print(output[0])
+        if output[0] < 3: #belongs in level 1
+            graph[1] = mask_i
+        else:
+            ls = [l for l in label_nums if l not in graph.keys()]
+
+    print(graph)
 
 def dfs(graph, start):
     visited, stack = set(), [start]
@@ -78,11 +86,11 @@ def dfs(graph, start):
 
 
 if __name__ == '__main__':
-	# Read the image you want connected components of
-	img = cv2.imread('a.png')
-	output = get_components(img)
-	print('components got')
-	nodes, labels, labeled_img = get_labeled_img(output)
-	print('labels got')
-	organize_components(nodes, labels, labeled_img)
-	print('stuff organized')
+    # Read the image you want connected components of
+    img = cv2.imread('a.png')
+    output = get_components(img)
+    print('components got')
+    nodes, labels, labeled_img = get_labeled_img(output)
+    print('labels got')
+    organize_components(nodes, labels, labeled_img)
+    print('stuff organized')
