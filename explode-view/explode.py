f0c5e7bdc8cd1463cbd562c33e3f0663722fe8a5
Sarah Kushner <sarahannekushner@yahoo.com>
2018-03-27 11:11:35 -0400

    explode view vis connected components. todo: get rid of background label

--- explode-view/explode.py
+++ explode-view/explode.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from collections import defaultdict
 
 def get_components(src):
     src_bw = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
@@ -38,42 +39,74 @@ def get_labeled_img(connected_comps):
     # set bg label to black
     labeled_img[label_hue==0] = 255
 
+    #kernel = np.ones((3,3),np.uint8)
+    #labeled_img = cv2.morphologyEx(labeled_img, cv2.MORPH_OPEN, kernel)
+    #labeled_img = cv2.morphologyEx(labeled_img, cv2.MORPH_OPEN, kernel)
+
+
     cv2.imwrite('components.png', labeled_img)
 
     nodes = dict()
     for i in range(num_labels):
         nodes[i] = labeled_img[label_hue==i]
 
-    output = (nodes,labels,labeled_img)
+    ret = (nodes,labels,labeled_img)
 
-    return output
+    return ret
 
+def make_levels(label_nums, labels, graph):
+    level_num = len(graph.keys())
+    background_mask = graph[level_num-1]
+    graph[level_num] = np.zeros_like(background_mask)
 
-def organize_components(nodes, labels, labeled_img):
-    graph = dict()
-    graph[0] = 0
-    # graph will have form
-    # graph[level number] : {comp0, comp1, comp2, ...}
-
-    label_nums = list(nodes.keys())
-    
-    background_mask = np.uint8(labels==0)
-    for i in label_nums:
-        print(i)
+    #print(len(label_nums))
+    for i,label in enumerate(label_nums):
+        #print(i)
 
         mask_i = np.uint8(labels==label_nums[i])
+        #print(mask_i.shape)
 
-        #mask_iplus1 = np.uint8(labels==label_nums[i+1])
         new_mask = background_mask+mask_i
         output = cv2.connectedComponentsWithStats(new_mask, 8, cv2.CV_32S)
 
-        print(output[0])
-        if output[0] < 3: #belongs in level 1
-            graph[1] = mask_i
-        else:
-            ls = [l for l in label_nums if l not in graph.keys()]
+        #print(output[0])
+        if output[0] < 3: #belongs in level
+            graph[level_num] += mask_i
+            label_nums.remove(label)
 
-    print(graph)
+def organize_components(nodes, labels, labeled_img):
+    background_mask = np.uint8(labels==0)
+
+    graph = dict()
+    graph[0] = background_mask
+    # graph will have form
+    # { level number : comp0 + comp1 + comp2 + ...] }
+
+    label_nums = list(nodes.keys())
+    label_nums.remove(0)
+
+    while label_nums:
+        make_levels(label_nums, labels, graph)
+
+    for level in graph.keys():
+        print(graph[level].shape)
+        
+        img = graph[level]
+
+        # Map component labels to hue val
+        label_hue = np.uint8(179*img/np.max(img))
+        blank_ch = 255*np.ones_like(label_hue)
+        labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+        print(labeled_img.shape)
+
+        # cvt to BGR for display
+        labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+
+        # set bg label to black
+        labeled_img[label_hue==0] = 255
+
+        cv2.imwrite('components'+str(level)+'.png', labeled_img)
+
 
 def dfs(graph, start):
     visited, stack = set(), [start]
@@ -93,4 +126,4 @@ if __name__ == '__main__':
     nodes, labels, labeled_img = get_labeled_img(output)
     print('labels got')
     organize_components(nodes, labels, labeled_img)
-    print('stuff organized')
+    print('levels got')
