fbc54f1ce1b15c3076365a2c233e0bd6dca726be
sarahanne <sak@dgp.toronto.edu>
2018-03-27 15:45:26 -0400

    explode view code is there. still some bugs to work out.

--- explode-view/explode.py
+++ explode-view/explode.py
@@ -43,7 +43,6 @@ def get_labeled_img(connected_comps):
     #labeled_img = cv2.morphologyEx(labeled_img, cv2.MORPH_OPEN, kernel)
     #labeled_img = cv2.morphologyEx(labeled_img, cv2.MORPH_OPEN, kernel)
 
-
     cv2.imwrite('components.png', labeled_img)
 
     nodes = dict()
@@ -56,18 +55,22 @@ def get_labeled_img(connected_comps):
 
 def make_levels(label_nums, labels, graph):
     level_num = len(graph.keys())
-    background_mask = graph[level_num-1]
-    graph[level_num] = np.zeros_like(background_mask)
+    background_mask = sum(graph[l] for l in graph.keys())
+    mask_0 = np.uint8(labels==0)
+    graph[level_num] = np.zeros_like(mask_0)
 
-    #print(len(label_nums))
     for i,label in enumerate(label_nums):
         #print(i)
 
         mask_i = np.uint8(labels==label_nums[i])
-        #print(mask_i.shape)
+        
+        new_mask = background_mask+mask_i-mask_0
 
-        new_mask = background_mask+mask_i
-        output = cv2.connectedComponentsWithStats(new_mask, 8, cv2.CV_32S)
+        kernel = np.ones((3,3),np.uint8)
+        new_mask = cv2.dilate(new_mask, kernel, iterations=2)
+
+        ret, thresh = cv2.threshold(new_mask,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        output = cv2.connectedComponentsWithStats(thresh, 8, cv2.CV_32S)
 
         #print(output[0])
         if output[0] < 3: #belongs in level
@@ -88,42 +91,45 @@ def organize_components(nodes, labels, labeled_img):
     while label_nums:
         make_levels(label_nums, labels, graph)
 
-    for level in graph.keys():
-        print(graph[level].shape)
-        
+    components = list()
+    for level in graph.keys(): 
+        #print(level)     
         img = graph[level]
 
-        # Map component labels to hue val
         label_hue = np.uint8(179*img/np.max(img))
         blank_ch = 255*np.ones_like(label_hue)
         labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
-        print(labeled_img.shape)
-
-        # cvt to BGR for display
         labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
 
-        # set bg label to black
-        labeled_img[label_hue==0] = 255
+        labeled_img[label_hue==0] = 0
+        if not(np.count_nonzero(labeled_img) == 0) and not(level==0):
+            #cv2.imwrite('components'+str(level)+'.png', labeled_img)
+            components.append(labeled_img)
 
-        cv2.imwrite('components'+str(level)+'.png', labeled_img)
+    return components
 
 
-def dfs(graph, start):
-    visited, stack = set(), [start]
-    while stack:
-        vertex = stack.pop()
-        if vertex not in visited:
-            visited.add(vertex)
-            stack.extend(graph[vertex] - visited)
-    return visited
+def combine_components(components):
+    even = np.zeros_like(components[0])
+    odd = np.zeros_like(components[0])
+    for i,comp in enumerate(components):
+        if i % 2 == 0:
+            even += comp
+        else:
+            odd += comp
+
+    cv2.imwrite('odd_components.png', odd)
+    cv2.imwrite('even_components.png', even)
 
 
 if __name__ == '__main__':
     # Read the image you want connected components of
     img = cv2.imread('a.png')
     output = get_components(img)
-    print('components got')
+    print('connected components got')
     nodes, labels, labeled_img = get_labeled_img(output)
     print('labels got')
-    organize_components(nodes, labels, labeled_img)
+    components = organize_components(nodes, labels, labeled_img)
     print('levels got')
+    combine_components(components)
+    print('combined conmponents got')
